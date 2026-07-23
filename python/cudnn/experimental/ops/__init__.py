5de7002e65300368319a121b63d99334080ae3c6
Rajas Mhaskar <rmhaskar@nvidia.com>
2026-07-23 18:50:34 +0000

    First commit for torch custom ops for LN/RMS norm

--- python/cudnn/experimental/ops/__init__.py
+++ python/cudnn/experimental/ops/__init__.py
@@ -1,7 +1,11 @@
 from .sdpa import scaled_dot_product_attention
 from .moe_grouped_matmul import moe_grouped_matmul
+from .rmsnorm import rms_norm
+from .layernorm import layer_norm
 
 __all__ = [
     "scaled_dot_product_attention",
     "moe_grouped_matmul",
+    "rms_norm",
+    "layer_norm",
 ]
